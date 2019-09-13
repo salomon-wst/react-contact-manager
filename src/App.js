815cd7b98c0ad93e6022109a10b0a2423cd400b6
salomon <salomon.websight@gmail.com>
2019-09-13 12:56:12 +0530

    form validation added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
 // import Contact from './components/Contact'
-import Contacts from './components/contacts/Contacts'
-import AddContact from './components/contacts/AddContact'
-import Header from './components/layouts/Header'
-import {Provider} from './context'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import Header from "./components/layouts/Header";
+import { Provider } from "./context";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
-  render()
-  {
+  render() {
     return (
       <Provider>
-      <div className="App">
-        <Header title='Contact Manager'></Header>
-        <div className="container">
-          <AddContact/>
-          <Contacts />
+        <div className="App">
+          <Header title="Contact Manager"></Header>
+          <div className="container">
+            <AddContact />
+            <Contacts />
+          </div>
         </div>
-      </div>
       </Provider>
     );
-
   }
 }
-
 export default App;
--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,76 +1,98 @@
-import React,{Component} from 'react';
-import {Consumer} from '../../context';
-import TextInputGroup from '../layouts/TextInputGroup';
-import uuid from 'uuid';
+import React, { Component } from "react";
+import { Consumer } from "../../context";
+import TextInputGroup from "../layouts/TextInputGroup";
+import uuid from "uuid";
 
 class AddContact extends Component {
-  state ={
-    name:'',
-    email:'',
-    phone:'',
-  }
-  onChange = (e) =>{
-    this.setState({[e.target.name]:e.target.value})
-  }
-  onSubmit = (dispatch,e) =>{
+  state = {
+    name: "",
+    email: "",
+    phone: "",
+    errors: {}
+  };
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onSubmit = (dispatch, e) => {
     e.preventDefault();
-    const {email,phone,name} = this.state;
-    const newContact={
-      id:uuid(),
+    const { email, phone, name } = this.state;
+    //Check for errors
+    if (name === "") {
+      this.setState({ errors: { name: "Name required" } });
+      return;
+    }
+    if (email === "") {
+      this.setState({ errors: { email: "Email required" } });
+      return;
+    }
+    if (phone === "") {
+      this.setState({ errors: { phone: "Phone required" } });
+      return;
+    }
+    const newContact = {
+      id: uuid(),
       name,
       email,
       phone
-    }
-    dispatch({type:'ADD_CONTACT',payload:newContact});
+    };
+    dispatch({ type: "ADD_CONTACT", payload: newContact });
+    //clear state
     this.setState({
-      name:'',
-      email:'',
-      phone:'',
+      name: "",
+      email: "",
+      phone: "",
+      errors: {}
     });
-  }
-  render(){
-    const {name,email,phone} = this.state;
-    return(
+  };
+  render() {
+    const { name, email, phone, errors } = this.state;
+    return (
       <Consumer>
-        {value=>{
-          const {dispatch} = value;
-        	return (
-      	    	<div className='card card-body mb-3'>
-      	    	  <div className='card-header'>Add Contact</div>
-                <div className='card-body'>
-                  <form onSubmit={this.onSubmit.bind(this,dispatch)}>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <div className="card-header">Add Contact</div>
+              <div className="card-body">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
                   <TextInputGroup
-                  label="Name"
-                  name="name"
-                  placeholder="Enter the name...."
-                  value={name}
-                  onChange={this.onChange}
-                   />
+                    label="Name"
+                    name="name"
+                    placeholder="Enter the name...."
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
                   <TextInputGroup
-                  label="Email"
-                  name="email"
-                  placeholder="Enter the email...."
-                  value={email}
-                  type="email"
-                  onChange={this.onChange}
-                   />
+                    label="Email"
+                    name="email"
+                    placeholder="Enter the email...."
+                    value={email}
+                    type="email"
+                    onChange={this.onChange}
+                    error={errors.email}
+                  />
                   <TextInputGroup
-                  label="Phone"
-                  name="phone"
-                  placeholder="Enter the phone...."
-                  value={phone}
-                  onChange={this.onChange}
-                   /> 
-                  <button type="submit" className="btn btn-block btn-light btn-primary">Add Contact</button>   
-                  </form>
-                </div>
-      	    	</div>
-      	    	);
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter the phone...."
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
+                  <button
+                    type="submit"
+                    className="btn btn-block btn-light btn-primary"
+                  >
+                    Add Contact
+                  </button>
+                </form>
+              </div>
+            </div>
+          );
         }}
       </Consumer>
-      )
-
-
+    );
   }
 }
 export default AddContact;
