93e48bf20159d225c51fa959477b2da4e32a6e8d
salomon <salomon.websight@gmail.com>
2019-09-15 18:35:37 +0530

    Updates

---
+++ precache-manifest.a9013281fefded525922a8b84b57bb9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8724f9e72e7d72b9f6c87ff36477c4",
+    "url": "/react-contact-manager/index.html"
+  },
+  {
+    "revision": "cc3f10f1322e1fc01f62",
+    "url": "/react-contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9d7d2f29f2ec8e988b29",
+    "url": "/react-contact-manager/static/css/main.3d611bd0.chunk.css"
+  },
+  {
+    "revision": "cc3f10f1322e1fc01f62",
+    "url": "/react-contact-manager/static/js/2.6c4768e4.chunk.js"
+  },
+  {
+    "revision": "9d7d2f29f2ec8e988b29",
+    "url": "/react-contact-manager/static/js/main.428a32e0.chunk.js"
+  },
+  {
+    "revision": "2c05f193f61043bfa7c5",
+    "url": "/react-contact-manager/static/js/runtime~main.e412cc12.js"
+  }
+]);
